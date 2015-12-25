d86f2c9ed8ddb31c759c1eefaf2e936508161bde
Duong Tran <tq.duong@icloud.com>
2015-12-25 19:37:51 +0700

    First draft of AngularJS framework

---
+++ angular-phonecat/test/unit/controllersSpec.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/* jasmine specs for controllers go here */
+
+describe('PhoneListCtrl', function() {
+
+    beforeEach(module('phonecatApp'));
+
+    it('should create phone model with 3 phones', inject(function($controller) {
+        var scope = {},
+            ctrl  = $controller('PhoneListCtrl', { $scope: scope });
+
+        expect(scope.phones.length).toBe(3);
+
+        expect(scope.name).toBe('World');
+    }))
+
+});
