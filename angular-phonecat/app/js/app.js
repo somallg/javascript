d32353f7c27285a4be78198385676df9a93c0611
somallg <tq.duong@icloud.com>
2015-12-26 01:20:50 +0700

    Update angularjs tutorial

--- angular-phonecat/app/js/app.js
+++ angular-phonecat/app/js/app.js
@@ -1,3 +1,20 @@
 'use strict';
 
 /* App Module */
+var phonecatApp = angular.module('phonecatApp',
+    [ 'ngRoute', 'phonecatAnimations','phonecatControllers', 'phonecatFilters', 'phonecatServices']);
+
+phonecatApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider.
+        when('/phones', {
+            templateUrl: 'partials/phone-list.html',
+            controller: 'PhoneListCtrl'
+        }).
+        when('/phones/:phoneId', {
+            templateUrl: 'partials/phone-detail.html',
+            controller: 'PhoneDetailCtrl'
+        }).
+        otherwise({
+            redirectTo: '/phones'
+        });
+}]);
--- angular-phonecat/test/unit/controllersSpec.js
+++ angular-phonecat/test/unit/controllersSpec.js
@@ -2,17 +2,43 @@
 
 /* jasmine specs for controllers go here */
 
-describe('PhoneListCtrl', function() {
+describe('PhoneCat controllers', function() {
+    describe('PhoneListCtrl', function() {
+        var scope, ctrl, $httpBackend;
 
-    beforeEach(module('phonecatApp'));
+        // Load our app module definition before each test
+        beforeEach(module('phonecatApp'));
 
-    it('should create phone model with 3 phones', inject(function($controller) {
-        var scope = {},
-            ctrl  = $controller('PhoneListCtrl', { $scope: scope });
+        beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+            $httpBackend = _$httpBackend_;
+            $httpBackend.expectGET('phones/phones.json')
+                .respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
 
-        expect(scope.phones.length).toBe(3);
+            scope = $rootScope.$new();
+            ctrl = $controller('PhoneListCtrl', { $scope: scope });
+        }));
 
-        expect(scope.name).toBe('World');
-    }))
+        it('should create "phones" model with 2 phones fetched from xhr', function() {
+            expect(scope.phones).toBeUndefined();
+            $httpBackend.flush();
 
+            expect(scope.phones).toEqual([{name: 'Nexus S'},
+                {name: 'Motorola DROID'}]);
+        });
+
+        it('should set the default value of orderProp model', function() {
+            expect(scope.orderProp).toBe('age');
+        });
+
+        //it('should create phone model with 3 phones', function() {
+        //    expect(scope.phones.length).toBe(3);
+        //
+        //});
+        //
+        //it('should set the default value of orderProp model', function() {
+        //    expect(scope.orderProp).toBe('age');
+        //});
+
+    });
 });
+
