558d6ee7615bd4b17679a006c67e813ad9b6617d
somallg <tq.duong@icloud.com>
2016-03-26 22:32:23 +0700

    Upading weather app

---
+++ weather/src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '6c186bd312fb6c44839158e1da4c8d1e';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
