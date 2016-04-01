15d057e30e6f4c43525f9f5fe9e6b046fc5f5e15
Duong Tran <tq.duong@icloud.com>
2016-04-01 18:11:29 +0700

    Adding local-weather app

---
+++ local-weather/src/components/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="well">React simple starter with sass</div>
+    );
+  }
+}
---
+++ local-weather/src/components/test.js
@@ -0,0 +1,11 @@
+export default () => {
+  return 'Test 1';
+}
+
+export default () => {
+  return 'Test 2';
+};
+
+export const test3 = () => {
+  return 'Test 3';
+};
