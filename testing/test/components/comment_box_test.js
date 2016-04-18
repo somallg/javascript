ab7f7faf1792dc7f183b67b5e8b507dd567c654c
somallg <tq.duong@icloud.com>
2016-04-18 21:27:59 +0700

    Adding testing

---
+++ testing/test/components/comment_box_test.js
@@ -0,0 +1,16 @@
+import { renderComponent, expect } from '../test_helper';
+import CommentBox from '../../src/components/comment_box';
+
+describe('CommentBox', () => {
+  it('has a text area', () => {
+    const component = renderComponent(CommentBox);
+
+    expect(component).to.have
+  });
+  
+  it('has a button', () => {
+    
+    
+  });
+
+});
