d6243c911652de76ebbbcba9e9119c12593f345f
somallg <tq.duong@icloud.com>
2016-06-14 00:28:16 +0700

    Add auth

---
+++ auth/server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const router = require('./router');
+const moongoose = require('mongoose');
+
+
+// DB Setup
+moongoose.connect('mongodb://localhost/auth');
+
+// App setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+
+router(app);
+
+// Server setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on:', port);
