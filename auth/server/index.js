a2548001851b115a0567214dc34042e199dc1f19
somallg <tq.duong@icloud.com>
2016-06-16 01:29:59 +0700

    Add CORS support

--- auth/server/index.js
+++ auth/server/index.js
@@ -5,13 +5,14 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const moongoose = require('mongoose');
-
+const cors = require('cors');
 
 // DB Setup
 moongoose.connect('mongodb://localhost/auth');
 
 // App setup
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
 
 router(app);
